813fdc6f18a51e1524134a2f0610522ec46ccc8b
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-27 21:06:58 +0530

    Added:As a Player would like to see the board

--- src/com/tictocgame/TicTacToeGame.java
+++ src/com/tictocgame/TicTacToeGame.java
@@ -33,10 +33,26 @@ public class TicTacToeGame {
         }
         return letter;
     }
+    //UC3:
+    public void showBoard(char[] board) {
+        /*
+          * Player would like to see the board so player can choose the valid cells to make
+            their move during player turn.
+          * Display the board on Console
+         */
+        System.out.println("Display the Current Board :-");
+        int i=1;
+        while (i < 10) {
+            System.out.println(board[i]+" "+board[i+1]+" "+board[i+2]);
+            i = i+3;
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Player Name : ");
+        System.out.println("Enter the Player Name : ");
         String playerName = scanner.nextLine();
 //UC1:
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
@@ -58,5 +74,14 @@ public class TicTacToeGame {
             computerLetter = 'X';
         System.out.println(playerName+ "'s Letter is "+playerLetter);
         System.out.println("Computer Player's Letter is "+computerLetter);
+//UC3:
+        /*
+          Method of Show Board
+         */
+        for (int i=0; i<10; i++) {
+            if (i != 0)
+                board[i] = '_';
+        }
+        ticTacToeGame.showBoard(board);
     }
 }
