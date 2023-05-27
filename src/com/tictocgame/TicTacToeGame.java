f8cf60f320d30a745645874988b2a665383e9be0
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-27 17:25:11 +0530

    Added:Player Determine Computer

--- src/com/tictocgame/TicTacToeGame.java
+++ src/com/tictocgame/TicTacToeGame.java
@@ -1,4 +1,5 @@
 package com.tictocgame;
+import java.util.Scanner;
 
 public class TicTacToeGame {
     //UC1:
@@ -14,11 +15,30 @@ public class TicTacToeGame {
                 board[i] = ' ';
             }
         }
-        System.out.println("Currently No one is playing the Game");
         return board;
+    }
+    //UC2:
+    public char chooseLetter() {
+        /*
+         * Taking input from User to choose either 'X' or 'O'.
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose the Letter 'X' or 'O' :-");
+        char letter = scanner.next().charAt(0);
+        if (letter == 'X' || letter =='O') {
+            System.out.println("Player Selected the Letter.");
+        }else {
+            System.out.println("Invalid Input");
+            letter = chooseLetter();
+        }
+        return letter;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Player Name : ");
+        String playerName = scanner.nextLine();
+//UC1:
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
         /*
          * Create a board of char[] of size 10 and
@@ -26,7 +46,17 @@ public class TicTacToeGame {
          */
         char[] board = new char[10];
         board = ticTacToeGame.CreateEmptyBoard(board);
-
+//UC2:
+        /*
+         * Player and Computer to choose a letter X or O
+         */
+        char playerLetter = ticTacToeGame.chooseLetter();
+        char computerLetter;
+        if (playerLetter == 'X' )
+            computerLetter = 'O';
+        else
+            computerLetter = 'X';
+        System.out.println(playerName+ "'s Letter is "+playerLetter);
+        System.out.println("Computer Player's Letter is "+computerLetter);
     }
 }
-
